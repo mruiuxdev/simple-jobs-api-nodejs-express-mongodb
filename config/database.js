058fcc65ef4a477c2b7b299a98eaf605506f7383
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-04-11 13:06:40 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 // mongodb://127.0.0.1:27017/jobs
-const connectDB = async () => await mongoose.connect(process.env.DB_LOCAL_URL);
+const connectDB = async () =>
+	await mongoose.connect(process.env.DB_LOCAL_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
 
 if (connectDB) {
 	console.log("Connected to MongoDB");
